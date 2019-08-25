a671525366609d2500a9646616c2a020ba65b21f
Pranav <pranav.varshney_cs18@gla.ac.in>
2019-08-25 09:50:03 +0530

    Added Solution

---
+++ section1/question1/src/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+
+	public int addTwoNumbers(int a, int b) {
+		int c;
+		c = a+b;
+		return c;
+	}
+}
+/*
+ * WARNING: DO NOT ADD A MAIN METHOD TO THIS FILE OR ANYWHERE ELSE!
+ * */
